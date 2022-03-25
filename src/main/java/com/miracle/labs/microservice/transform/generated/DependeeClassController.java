70897c0321a1df909296e0fdcba7a153906e638c
codageeksdemo <support@codageeks.com>
2022-03-26 00:14:10 +0530

    Transform Code

--- src/main/java/com/miracle/labs/microservice/transform/generated/DependeeClassController.java
+++ src/main/java/com/miracle/labs/microservice/transform/generated/DependeeClassController.java
@@ -20,15 +20,15 @@ com.monolith.legacy.DependeeClassBridge target = new com.monolith.legacy.Depende
 public String setInternals(@RequestParam("0") String arg0 ,@RequestParam("1") String arg1 ,@RequestParam("2") String arg2 ,@RequestParam("3") String arg3 ,@RequestParam("4") String arg4 ){
 	return target.setInternals(arg0,arg1,arg2,arg3,arg4);
 }
-@GetMapping
-( path="/Internals/{arg0}", produces="application/json")
-public String getInternals(@PathVariable("arg0") String arg0 ){
-	return target.getInternals(arg0);
-}
 @PostMapping
 ( path="/A", produces="application/json")
 public String setA(@RequestParam("0") String arg0 ){
 	return target.setA(arg0);
+}
+@GetMapping
+( path="/Internals/{arg0}", produces="application/json")
+public String getInternals(@PathVariable("arg0") String arg0 ){
+	return target.getInternals(arg0);
 }
 @GetMapping
 ( path="/ProtectedInt", produces="application/json")
@@ -36,14 +36,14 @@ public String getProtectedInt(){
 	return target.getProtectedInt();
 }
 @GetMapping
-( path="/String", produces="application/json")
-public String getString(){
-	return target.getString();
-}
-@GetMapping
 ( path="/A", produces="application/json")
 public String getA(){
 	return target.getA();
+}
+@GetMapping
+( path="/String", produces="application/json")
+public String getString(){
+	return target.getString();
 }
 @GetMapping
 ( path="/Int", produces="application/json")
